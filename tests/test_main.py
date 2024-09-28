e1ab159a7aa81011d2a8fb8ad22c17e12dfca2ba
Владимир Сулым <v.sulym@gmail.com>
2024-09-28 17:52:09 +0300

    Tests cover 100%

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,8 @@
+from unittest.mock import patch
+
+from main import fill_class_with_data, read_data_from_json
+
+
 def test_product_init(product_iphone):
     assert product_iphone.name == "Iphone 15"
     assert product_iphone.description == "512GB, Gray space"
@@ -15,3 +20,17 @@ def test_category_init(category_smartphone):
 
     assert category_smartphone.category_count == 1
     assert category_smartphone.product_count == 1
+
+
+@patch("main.Category")
+def test_fill_class_with_data(mock_category, products_for_test):
+    mock_category.return_value = 1
+    assert fill_class_with_data(products_for_test) == [1, 1]
+
+
+@patch("json.load")
+@patch("builtins.open")
+def test_read_data_from_json(mock_open, mock_load):
+    mock_open.return_value.__iter__.return_value = []
+    mock_load.return_value = 1
+    assert read_data_from_json("products.json") == 1
