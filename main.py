5ceba9ce0329ba8ef4e5f4028ed5ef689d1e039c
Владимир Сулым <v.sulym@gmail.com>
2024-09-29 23:09:00 +0300

    Task 14_2 task 1, 2 and 3 completed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import json
 import os
 
-
 class Product:
     name: str
     description: str
@@ -14,6 +13,11 @@ class Product:
         self.price = price
         self.quantity = quantity
 
+    @classmethod
+    def new_product(cls, product):
+        name, description, price, quantity = product.values()
+        return cls(name, description, price, quantity)
+
 
 class Category:
     name: str
@@ -25,9 +29,21 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+    @property
+    def add_product(self):
+        result = []
+        for product in self.__products:
+            result.append(f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.')
+        return result
+
+    @add_product.setter
+    def add_product(self, value):
+        self.__products.append(value)
+        Category.product_count += 1
 
 
 path_file_date = os.path.join(os.path.dirname(__file__), "data", "products.json")
@@ -50,7 +66,7 @@ def fill_class_with_data(categories):
     return result
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 #     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
 #     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
 #     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
@@ -96,3 +112,37 @@ def fill_class_with_data(categories):
 #
 #     print(Category.category_count)
 #     print(Category.product_count)
+
+##### Задание 14_2
+
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3]
+    )
+
+    # print(category1.products)
+    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+    category1.add_product = product4
+    # print(category1.products)
+    # print(category1.product_count)
+
+    new_product = Product.new_product(
+        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+         "quantity": 5})
+    print(new_product.name)
+    print(new_product.description)
+    print(new_product.price)
+    print(new_product.quantity)
+
+    new_product.price = 800
+    print(new_product.price)
+
+    new_product.price = -100
+    print(new_product.price)
+    new_product.price = 0
+    print(new_product.price)
