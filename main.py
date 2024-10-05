c9980116626f13acd9c4fdc6c95f83ae319434c7
Владимир Сулым <v.sulym@gmail.com>
2024-10-05 11:31:30 +0300

    Task 15_1 task 1 and 2 completed

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
+    def __str__(self):
+        return f'{self.name}, {self.price} руб. Остаток: {self.quantity} шт.'
+
+    def __add__(self, other):
+        return self.quantity + other.quantity
+
     @classmethod
     def new_product(cls, value):
         name, description, price, quantity = value.values()
@@ -56,12 +62,18 @@ class Category:
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
 
+    def __str__(self):
+        number_of_products = 0
+        for product in self.__products:
+            number_of_products += product.quantity
+        return f'{self.name}, количество продуктов: {number_of_products} шт.'
+
     def add_product(self, value):
         self.__products.append(value)
         Category.product_count += 1
 
     @property
-    def product(self):
+    def products(self):
         result = []
         for product in self.__products:
             result.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.")
@@ -93,7 +105,7 @@ def fill_class_with_data(categories):
     return result
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 #     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
 #     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
 #     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
@@ -182,3 +194,28 @@ def fill_class_with_data(categories):
 # print(new_product.description)
 # print(new_product.price)
 # print(new_product.quantity)
+
+# ### Заданиие 15_1
+
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    # print(str(product1))
+    # print(str(product2))
+    # print(str(product3))
+
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3]
+    )
+
+    print(str(category1))
+
+    print(category1.products)
+
+    print(product1 + product2)
+    print(product1 + product3)
+    print(product2 + product3)
+
