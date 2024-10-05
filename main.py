bd24d4ec2e4221d18215a0a64cbeee711f29dc3c
Владимир Сулым <v.sulym@gmail.com>
2024-10-05 11:43:07 +0300

    Applied the black function

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class Product:
         self.quantity = quantity
 
     def __str__(self):
-        return f'{self.name}, {self.price} руб. Остаток: {self.quantity} шт.'
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
     def __add__(self, other):
         return self.quantity + other.quantity
@@ -66,7 +66,7 @@ class Category:
         number_of_products = 0
         for product in self.__products:
             number_of_products += product.quantity
-        return f'{self.name}, количество продуктов: {number_of_products} шт.'
+        return f"{self.name}, количество продуктов: {number_of_products} шт."
 
     def add_product(self, value):
         self.__products.append(value)
@@ -105,7 +105,7 @@ def fill_class_with_data(categories):
     return result
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 #     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
 #     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
 #     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
@@ -197,25 +197,24 @@ if __name__ == "__main__":
 
 # ### Заданиие 15_1
 
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+# product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+# product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+# product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
-    # print(str(product1))
-    # print(str(product2))
-    # print(str(product3))
+# print(str(product1))
+# print(str(product2))
+# print(str(product3))
 
-    category1 = Category(
-        "Смартфоны",
-        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [product1, product2, product3]
-    )
-
-    print(str(category1))
-
-    print(category1.products)
-
-    print(product1 + product2)
-    print(product1 + product3)
-    print(product2 + product3)
-
+# category1 = Category(
+#     "Смартфоны",
+#     "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+#     [product1, product2, product3]
+# )
+#
+# print(str(category1))
+#
+# print(category1.products)
+#
+# print(product1 + product2)
+# print(product1 + product3)
+# print(product2 + product3)
